3805fe4d99c4458a4d909f810422a16daf809e91
chandrafebri <chandra.febri14@gmail.com>
2022-01-12 20:46:25 +0800

    Upload Project

---
+++ src/main/java/com/mycompany/uas/Main.java
@@ -0,0 +1,20 @@
+package com.mycompany.uas;
+
+import com.mycompany.uas.model.Barang;
+import com.mycompany.uas.model.JenisBarang;
+import com.mycompany.uas.view.admin.BarangViewFrame;
+import com.mycompany.uas.view.admin.JenisBarangViewFrame;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello");
+        
+        BarangViewFrame barangViewFrame = new BarangViewFrame();
+        barangViewFrame.setVisible(true);
+//        
+//        JenisBarangViewFrame jenisBarangViewFrame = new JenisBarangViewFrame();
+//        jenisBarangViewFrame.setVisible(true);
+    }
+}
---
+++ src/main/java/com/mycompany/uas/db/Database.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.uas.db;
+
+/**
+ *
+ * @author gepengbro
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+    private final String URL = "jdbc:mysql://localhost:3306/";
+    private final String DB_NAME = "praktikum_penjualan";
+    private final String USER = "root";
+    private final String PASS = "";
+    
+    public Connection getConnection(){
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(URL+DB_NAME, USER, PASS);
+            System.out.println("Koneksi Berhasil");
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println(ex.toString());
+        }
+        return con;
+    }
+}
